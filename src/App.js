f4110e238c253f05859b72d2bf79adc8e2484d75
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-06-01 18:59:35 +0100

    Rendered the word menaings

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"> Hello</header>
+        <header className="App-header">
+          {" "}
+          <h1>Dictionary</h1>
+        </header>
         <Dictionary />
       </div>
       <footer className="App-footer">
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+    // console.log(response.data[0].meanings[0].definitions[0].definition)
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
 
-    function handleResponse(response) {
-      console.log(response);
-    }
     //documentation = https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -26,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
